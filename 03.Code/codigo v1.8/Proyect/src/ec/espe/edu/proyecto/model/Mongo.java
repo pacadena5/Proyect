ce7f0b562f40a85351a0fae849611b01f5e20c4e
pacadena5 <pacadena5@espe.edu.ec>
2020-01-13 00:11:05 -0500

    mongoDB

---
+++ 03.Code/codigo v1.8/Proyect/src/ec/espe/edu/proyecto/model/Mongo.java
@@ -0,0 +1,19 @@
+*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.espe.edu.proyecto.model;
+
+/**
+ *
+ * @author jcadena
+ */
+public class Mongo {
+    private String name;
+    private String password;
+    private String type;
+
+    public Mongo() {
+    }
+}
